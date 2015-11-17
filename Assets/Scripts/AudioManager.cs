0b44ef4fa91bae16cc9e859ada043a177e60ca77
Xavier Salleras <xavier.salleras@gmail.com>
2015-11-17 10:07:48 +0100

    DIMARTS

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject audioGameObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        for(int i = 0; i < audioGameObject.transform.childCount; i++)
+        {
+            float dist = Vector3.Distance(audioGameObject.transform.GetChild(i).position, player.transform.position);
+            float defVolume = 1 / dist * 20; // 20 * Mathf.Log10(dist);
+            audioGameObject.transform.GetChild(i).GetComponent<AudioSource>().outputAudioMixerGroup.audioMixer.SetFloat("volume", defVolume);
+        }
+	}
+}
